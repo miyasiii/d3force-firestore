2617dec6b4eddeee8c56dd931427b6c6c9b56263
miyasiii <hiro.miya.91@gmail.com>
2020-09-22 16:02:13 +0900

    uncomment firebase process

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
-// const firestore = firebase.firestore();
-// const docLinksRef = firestore.collection("links");
-// const docNodesRef = firestore.collection("nodes");
+const firestore = firebase.firestore();
+const docLinksRef = firestore.collection("links");
+const docNodesRef = firestore.collection("nodes");
 let width = document.body.clientWidth;
 let height = document.body.clientHeight;
 let links = [];
@@ -11,11 +11,11 @@ const dummyItems = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 for(let i=0; i<dummyItems.length; i++){
   nodes.push({id: dummyItems[i]});
 }
-for(let i=0; i<500; i++){
-  const src = dummyItems[Math.floor(dummyItems.length * Math.random())];
-  const dst = dummyItems[Math.floor(dummyItems.length * Math.random())];
-  links.push({source: src, target: dst});
-}
+// for(let i=0; i<500; i++){
+//   const src = dummyItems[Math.floor(dummyItems.length * Math.random())];
+//   const dst = dummyItems[Math.floor(dummyItems.length * Math.random())];
+//   links.push({source: src, target: dst});
+// }
 
 let canvasButton = d3.select("body").append("button")
   .text("canvas")
@@ -27,18 +27,18 @@ let canvasButton = d3.select("body").append("button")
     canvasButton.remove();
     svgButton.remove();
 
-    // const [ linkDocs, nodeDocs ] = await Promise.all([
-    //   docLinksRef.get().then(function(querySnapshot) {
-    //     querySnapshot.forEach(function(doc) {
-    //       links.push({source: doc.data().source, target: doc.data().target});
-    //     });
-    //   }),
-    //   docNodesRef.get().then(function(querySnapshot) {
-    //     querySnapshot.forEach(function(doc) {
-    //       nodes.push({id: doc.id});
-    //     });
-    //   })
-    // ])
+    const [ linkDocs, nodeDocs ] = await Promise.all([
+      docLinksRef.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          links.push({source: doc.data().source, target: doc.data().target});
+        });
+      }),
+      docNodesRef.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          nodes.push({id: doc.id});
+        });
+      })
+    ])
 
     const module = await import('./ForceSimulationGraphCanvas.js');
     forceGraph = new module.ForceSimulationGraphCanvas("app", "view", width, height);
@@ -57,18 +57,18 @@ let svgButton = d3.select("body").append("button")
     canvasButton.remove();
     svgButton.remove();
 
-    // const [ linkDocs, nodeDocs ] = await Promise.all([
-    //   docLinksRef.get().then(function(querySnapshot) {
-    //     querySnapshot.forEach(function(doc) {
-    //       links.push({source: doc.data().source, target: doc.data().target});
-    //     });
-    //   }),
-    //   docNodesRef.get().then(function(querySnapshot) {
-    //     querySnapshot.forEach(function(doc) {
-    //       nodes.push({id: doc.id});
-    //     });
-    //   })
-    // ])
+    const [ linkDocs, nodeDocs ] = await Promise.all([
+      docLinksRef.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          links.push({source: doc.data().source, target: doc.data().target});
+        });
+      }),
+      docNodesRef.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          nodes.push({id: doc.id});
+        });
+      })
+    ])
 
     const module = await import('./ForceSimulationGraphSVG.js');
     forceGraph = new module.ForceSimulationGraphSVG("app", "view", width, height);
